73cfa2ff1b4288ad9a3e7b19438e833b845a4372
OkapiGG <alancervantes800@gmail.com>
2026-02-16 20:47:35 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/compsql/CompSQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compsql;
+
+/**
+ *
+ * @author alancervantes
+ */
+public class CompSQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
